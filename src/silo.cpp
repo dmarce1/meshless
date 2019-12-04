7913f027fe35e8960294aecb7ddeca9fa2142d48
Dominic Marcello <dmarce504@gmail.com>
2019-12-04 14:59:44 -0600

    Added missing source

---
+++ src/silo.cpp
@@ -0,0 +1,50 @@
+/*
+ * silo.cpp
+ *
+ *  Created on: Dec 4, 2019
+ *      Author: dmarce1
+ */
+
+#include "delaunay.hpp"
+#include <silo.h>
+
+void output_silo(const std::vector<particle> &parts, const char *filename) {
+	DBfile *db = DBCreateReal(filename, DB_CLOBBER, DB_LOCAL, "Meshless", DB_HDF5);
+#if( NDIM == 1)
+	const int shapetypes[1] = {[DB_ZONETYPE_BEAM};
+#elif( NDIM ==2)
+	const int shapetypes[1] = { DB_ZONETYPE_TRIANGLE };
+#else
+	const int shapetypes[1] = {DB_ZONETYPE_TET};
+#endif
+	const int shapesize[1] = { NDIM + 1 };
+	const auto delaunay_regions = compute_delaunay_regions(parts);
+	const int nzones = delaunay_regions.size();
+	const int nnodes = parts.size();
+	const int shapecount[1] = { nzones };
+	std::vector<int> node_list;
+	node_list.reserve(NDIM * delaunay_regions.size());
+	for (const auto r : delaunay_regions) {
+		for (int n = 0; n < NDIM + 1; n++) {
+			node_list.push_back(r[n]);
+		}
+	}
+	real* coords[NDIM];
+	char* coordnames[NDIM];
+	for (int dim = 0; dim < NDIM; dim++) {
+		coords[dim] = new real[nnodes];
+		coordnames[dim] = new char[2];
+		coordnames[dim][0] = 'x' + dim;
+		coordnames[dim][1] = '\0';
+		for (int i = 0; i < nnodes; i++) {
+			coords[dim][i] = parts[i].x[dim];
+		}
+	}
+	DBPutZonelist2(db, "zonelist", nzones, NDIM, node_list.data(), node_list.size(), 0, 0, 0, shapetypes, shapesize, shapecount, 1, NULL);
+	DBPutUcdmesh(db, "mesh", NDIM, coordnames, coords, nnodes, nzones, "zonelist", NULL, DB_DOUBLE, NULL);
+	for (int dim = 0; dim < NDIM; dim++) {
+		delete[] coordnames[dim];
+		delete[] coords[dim];
+	}
+	DBClose(db);
+}
---
+++ src/silo.hpp
@@ -0,0 +1,16 @@
+/*
+ * silo.hpp
+ *
+ *  Created on: Dec 4, 2019
+ *      Author: dmarce1
+ */
+
+#ifndef SRC_SILO_HPP_
+#define SRC_SILO_HPP_
+
+#include "particle.hpp"
+
+void output_silo(const std::vector<particle>&, const char*);
+
+
+#endif /* SRC_SILO_HPP_ */
